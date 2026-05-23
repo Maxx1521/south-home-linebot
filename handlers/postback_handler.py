9347a1a1b11d712f41f6a5e3464df920d96f29eb
Maxx1521 <z4791521@gmail.com>
2026-05-23 22:20:20 +0800

    fix: simplify store visit prompt text

--- handlers/postback_handler.py
+++ handlers/postback_handler.py
@@ -35,7 +35,7 @@ def handle_postback(event, line_bot_api):
             reply = start_booking(appt_type="門市參觀", store=store, user_id=user_id)
         else:
             reply = TextMessage(
-                text="🏠 門市參觀\n\n請問您想預約哪間門市？",
+                text="請問您想預約哪間門市？",
                 quick_reply=QuickReply(items=[
                     QuickReplyItem(action=PostbackAction(label="左營南屏店", data="action=store_visit&store=左營店", display_text="左營南屏店")),
                     QuickReplyItem(action=PostbackAction(label="三民大昌店", data="action=store_visit&store=三民店", display_text="三民大昌店")),
